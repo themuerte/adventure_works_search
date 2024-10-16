0dd6f68ba1f6ac886d08fd2eb0b473f0896495a6
Eduardo Lopez <eduardol.rostran@gmail.com>
2024-10-16 11:12:20 -0600

    add project

---
+++ src/test/java/com/example/adventure_works_search/AdventureWorksSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adventure_works_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureWorksSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
